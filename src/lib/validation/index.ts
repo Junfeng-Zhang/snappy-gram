0c18d0785c5d04344a70ed11c59eeffed8cdaa27
Junfeng-Zhang <60849891+Junfeng-Zhang@users.noreply.github.com>
2023-12-19 12:48:43 +0800

    fix validation contents

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,8 +2,8 @@ import * as z from "zod";
 
 // 注册校验
 export const SignupValidation = z.object({
-  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
-  username: z.string().min(2, { message: "Name must be at least 2 characters." }),
+  name: z.string().min(2, { message: "名称至少包含2个字符。" }),
+  username: z.string().min(2, { message: "用户名至少包含2个字符。" }),
   email: z.string().email(),
   password: z.string().min(8, { message: "密码必须至少8个字符。" }),
 });
@@ -11,14 +11,14 @@ export const SignupValidation = z.object({
 // 登录校验
 export const SigninValidation = z.object({
   email: z.string().email(),
-  password: z.string().min(8, { message: "密码必须至少8个字符。" }),
+  password: z.string().min(8, { message: "密码长度不得小于8个字符。" }),
 });
 
 // 资料校验
 export const ProfileValidation = z.object({
   file: z.custom<File[]>(),
-  name: z.string().min(2, { message: "名称必须至少包含 2 个字符。" }),
-  username: z.string().min(2, { message: "名称必须至少包含 2 个字符。" }),
+  name: z.string().min(2, { message: "名称必须至少包含2个字符。" }),
+  username: z.string().min(2, { message: "用户名必须至少包含2个字符。" }),
   email: z.string().email(),
   bio: z.string(),
 });
